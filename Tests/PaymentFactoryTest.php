bd19246dbf1e3c14f0330b273ea8b3efa3cf3a22
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-12-16 20:46:06 +0000

    [payment-factories] finish redesign.

---
+++ Tests/PaymentFactoryTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Payum\Offline\Tests;
+
+use Payum\Offline\PaymentFactory;
+
+class PaymentFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldImplementPaymentFactoryInterface()
+    {
+        $rc = new \ReflectionClass('Payum\Offline\PaymentFactory');
+
+        $this->assertTrue($rc->implementsInterface('Payum\Core\PaymentFactoryInterface'));
+    }
+
+    /**
+     * @test
+     */
+    public function couldBeConstructedWithoutAnyArguments()
+    {
+        new PaymentFactory();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAllowCreatePayment()
+    {
+        $factory = new PaymentFactory();
+
+        $payment = $factory->create();
+
+        $this->assertInstanceOf('Payum\Core\Payment', $payment);
+        
+        $this->assertAttributeEmpty('apis', $payment);
+        $this->assertAttributeNotEmpty('actions', $payment);
+
+        $extensions = $this->readAttribute($payment, 'extensions');
+        $this->assertAttributeNotEmpty('extensions', $extensions);
+    }
+}
