17dd33890945602908c42294c4fcac0f67d6f1c6
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2015-04-06 08:02:46 +0000

    rename payment to gateway

--- OfflineGatewayFactory.php
+++ OfflineGatewayFactory.php
@@ -2,18 +2,18 @@
 namespace Payum\Offline;
 
 use Payum\Core\Bridge\Spl\ArrayObject;
-use Payum\Core\PaymentFactory as CorePaymentFactory;
-use Payum\Core\PaymentFactoryInterface;
+use Payum\Core\GatewayFactory as CoreGatewayFactory;
+use Payum\Core\GatewayFactoryInterface;
 use Payum\Offline\Action\CaptureAction;
 use Payum\Offline\Action\FillOrderDetailsAction;
 use Payum\Offline\Action\StatusAction;
 
-class PaymentFactory implements PaymentFactoryInterface
+class OfflineGatewayFactory implements GatewayFactoryInterface
 {
     /**
-     * @var PaymentFactoryInterface
+     * @var GatewayFactoryInterface
      */
-    protected $corePaymentFactory;
+    protected $coreGatewayFactory;
 
     /**
      * @var array
@@ -22,11 +22,11 @@ class PaymentFactory implements PaymentFactoryInterface
 
     /**
      * @param array $defaultConfig
-     * @param PaymentFactoryInterface $corePaymentFactory
+     * @param GatewayFactoryInterface $coreGatewayFactory
      */
-    public function __construct(array $defaultConfig = array(), PaymentFactoryInterface $corePaymentFactory = null)
+    public function __construct(array $defaultConfig = array(), GatewayFactoryInterface $coreGatewayFactory = null)
     {
-        $this->corePaymentFactory = $corePaymentFactory ?: new CorePaymentFactory();
+        $this->coreGatewayFactory = $coreGatewayFactory ?: new CoreGatewayFactory();
         $this->defaultConfig = $defaultConfig;
     }
 
@@ -35,7 +35,7 @@ class PaymentFactory implements PaymentFactoryInterface
      */
     public function create(array $config = array())
     {
-        return $this->corePaymentFactory->create($this->createConfig($config));
+        return $this->coreGatewayFactory->create($this->createConfig($config));
     }
 
     /**
@@ -45,7 +45,7 @@ class PaymentFactory implements PaymentFactoryInterface
     {
         $config = ArrayObject::ensureArrayObject($config);
         $config->defaults($this->defaultConfig);
-        $config->defaults($this->corePaymentFactory->createConfig((array) $config));
+        $config->defaults($this->coreGatewayFactory->createConfig((array) $config));
 
         $config->defaults(array(
             'payum.factory_name' => 'offline',
